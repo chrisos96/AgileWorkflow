275bb2a0fda81726701c71120c9f7105ed8c588a
Chris O Shea <christoper.oshea@mycit.ie>
2020-12-08 12:41:10 +0000

    added simple arithmetic example

--- HelloPython.py
+++ HelloPython.py
@@ -2,6 +2,9 @@ import datetime
 print("Hello world!")
 print("hi!")
 print("Hello there")
+x=5
+y=5
+print(x+y)
 now = datetime.datetime.now()
 print("Current date and time is ")
 print(now.strftime("%A, %d-%m-%Y : %H:%M"))
