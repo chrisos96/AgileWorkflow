abade412604f0c6e2e6135c4c1d2c354f8d2b304
Chris O Shea <christoper.oshea@mycit.ie>
2020-12-08 12:32:21 +0000

    added another new print statement

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,7 @@
 import datetime
 print("Hello world!")
-print("hi")
+print("hi!")
+print("Hello there")
 now = datetime.datetime.now()
 print("Current date and time is ")
 print(now.strftime("%A, %d-%m-%Y : %H:%M"))
