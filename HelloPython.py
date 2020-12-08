9ecd1c9885ec06d336243f04072fb8e4b02158a6
Chris O Shea <christoper.oshea@mycit.ie>
2020-12-08 16:50:22 +0000

    good build December 8th

--- HelloPython.py
+++ HelloPython.py
@@ -6,7 +6,7 @@ print("Testing workflow example")
 x=5
 y=5
 print(x+y)
-prit here
+print("Here")
 now = datetime.datetime.now()
 print("Current date and time is ")
 print(now.strftime("%A, %d-%m-%Y : %H:%M"))
